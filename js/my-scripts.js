67dd30a1c3177b2b690ef0a3316d975fd7c4dee7
natei <rinat465935387@gmail.com>
2024-07-26 06:48:20 +0500

    настройка анимации

--- js/my-scripts.js
+++ js/my-scripts.js
@@ -2,19 +2,41 @@
 window.onscroll = function() {fixTop()};
 var header = document.getElementById("page-header");
 function fixTop(){
-    if (window.pageYOffset > 50) {
-    header.classList.add("sticky-header");
-}
-    else {
-        header.classList.remove("sticky-header");
-    }
-}
+    if (window.pageYOffset > 50) {header.classList.add("sticky-header");}
+    else {header.classList.remove("sticky-header");}}
 
 /* Скрипт для правой и левой кнопки меню, чтоб был активен только 1 или нисколько */
-var Ch1 = document.getElementById('check-ID1');
-var Ch2 = document.getElementById('check-ID2');
+var Ch1 = document.getElementById('menu-button-left');
+var Ch2 = document.getElementById('menu-button-right');
 
-Ch1.onclick = function() {
-    document.getElementById('check-ID2').checked=false;}
-Ch2.onclick = function() {
-    document.getElementById('check-ID1').checked=false;}
+Ch1.onclick = function() {document.getElementById('menu-button-right').checked=false;}
+Ch2.onclick = function() {document.getElementById('menu-button-left').checked=false;}
+
+/* Скрипт настроек для слайдера Owl-carusel */
+$(document).ready(function(){
+    const slider = $("#slider").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:false,
+        smartSpeed:1000,
+        autoplayTimeout:7000,
+        nav:false,
+        dots:false,
+        center: true,		
+    });
+});
+    const slider_feedback = $("#slider-feedback").owlCarousel({
+        loop:true,
+        autoplay:true,
+        smartSpeed:1000,
+        autoplayTimeout:7000,
+        nav:false,
+        dots:false,
+        center: false,
+        responsive:{
+            0:{items:1},
+            800:{items:2},
+            1200:{items:3},
+        }
+    });
+    
